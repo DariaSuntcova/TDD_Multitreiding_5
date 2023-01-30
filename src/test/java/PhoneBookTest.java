d219c7d141ae33a8264971aa2a2026c251bf8d7c
Daria Suntcova <batareyka23@gmail.com>
2023-01-30 14:06:34 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,33 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @ParameterizedTest
+    @CsvSource({
+            "Ivan, 221955, true",
+            "Kerry, 89198525698, true",
+            "Mama, +79501568741, true",
+            "Daria, null, false",
+            "null, 452369, false",
+            "null, null, false"
+    })
+    public void shouldAdd(String name, String number, boolean expected) {
+
+        Assertions.assertEquals(expected, phoneBook.add(name, number));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Ivan, 221955",
+    })
+    public void shouldAddSameName(String name, String number) {
+        phoneBook.add(name, number);
+
+        Assertions.assertFalse(phoneBook.add(name, number));
+    }
+
 }
